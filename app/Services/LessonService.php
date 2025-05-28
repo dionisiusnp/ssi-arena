51183e3630faef66b03e512db32fbe766867f64d
Yon <dionisiusn1@gmail.com>
2025-05-28 16:19:36 +0700

    update

--- app/Services/LessonService.php
+++ app/Services/LessonService.php
@@ -2,13 +2,21 @@
 
 namespace App\Services;
 
+use App\Models\Lesson;
+
 class LessonService
 {
+    protected $model;
     /**
      * Create a new class instance.
      */
-    public function __construct()
+    public function __construct(Lesson $lesson)
     {
-        //
+        $this->model = $lesson;
+    }
+
+    public function model()
+    {
+        return $this->model;
     }
 }
--- app/Services/RoadmapService.php
+++ app/Services/RoadmapService.php
@@ -2,13 +2,21 @@
 
 namespace App\Services;
 
+use App\Models\Roadmap;
+
 class RoadmapService
 {
+    protected $model;
     /**
      * Create a new class instance.
      */
-    public function __construct()
+    public function __construct(Roadmap $roadmap)
     {
-        //
+        $this->model = $roadmap;
+    }
+
+    public function model()
+    {
+        return $this->model;
     }
 }
