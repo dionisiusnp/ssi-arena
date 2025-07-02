b5f303af3e6221f1816979696d2d2e3f7972f851
Dionisius NP <dionisiusn1@gmail.com>
2025-07-02 10:08:22 +0700

    update

--- app/Models/Setting.php
+++ app/Models/Setting.php
@@ -2,9 +2,17 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Setting extends Model
 {
-    //
+    use HasFactory, SoftDeletes;
+    protected $guarded = ['id', 'created_at', 'updated_at', 'deleted_at'];
+
+    public function lastChanger()
+    {
+        return $this->belongsTo(User::class,'changed_by');
+    }
 }
